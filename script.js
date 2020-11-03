3ca6d1305a27a1fe2f8c4ea7903787655292f9be
henriB <henri.bretseau@gmail.com>
2020-11-03 12:33:18 +0100

    data en tableau

---
+++ script.js
@@ -0,0 +1,6 @@
+const passwordOutput = document.getElementById('password-output');
+const dataLowercase = 'azertyuiopqsdfghjklmwxcvbn'.split('');
+const dataLowercase = 'AZERTYUIOPQSDFGHJKLMWXCVBN'.split('');
+const dataNumbers = '0123456789'.split('');
+const dataSymbols = "!@#$%^&*-_=+\|:;',.>/?~".split('');
+
